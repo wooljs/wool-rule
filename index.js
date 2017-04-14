1ecddd68a6a139784a99f4ebc9eddfaaff2558c5
Nicolas Lochet <nlo@connecthings.com>
2017-04-14 10:57:13 +0200

    include wool-state as dependency

--- index.js
+++ index.js
@@ -9,4 +9,8 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-module.exports = (function() {}())
+module.exports = (function() {
+  'use strict'
+  
+
+}())
