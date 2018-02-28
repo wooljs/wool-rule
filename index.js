b8a7f365d950b0ebcb321ce61609e05e2187a4bd
Nicolas Lochet <nicolas.lochet@gmail.com>
2018-02-28 16:18:19 +0100

    clean to release

--- index.js
+++ index.js
@@ -9,75 +9,8 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-module.exports = (function() {
-  'use strict'
-  var Store = require('wool-store')
-
-  function Rule(r, s) {
-    if (! (this instanceof Rule)) return new Rule(r, s)
-    this.rules = r.reduce(function(p, c) {
-      var x = p[c.n] = {}
-      x.n = c.n
-      x.p = c.p
-      if ('c' in c) { x.c = c.c.bind(this) }
-      x.o = c.o.bind(this)
-      return p
-    }.bind(this), {})
-    this.store = s || Store()
-  }
-  Rule.prototype.push = function(cmd, cb) {
-    var rule = this.rules[cmd.n]
-    if ('c' in rule) {
-      return rule.c(cmd.t, cmd.d, function(err) {
-        if (err) return cb(err)
-        else try {
-          return this.execute(rule, cmd, cb)
-        } catch (e) {
-          return cb(e)
-        }
-      }.bind(this))
-    } else {
-      return this.execute(rule, cmd, cb)
-    }
-  }
-  Rule.prototype.execute = function(rule, cmd, cb) {
-    return rule.o(cmd.t, cmd.d, function(err, data) {
-      if (err) return cb(err)
-      else try {
-        return cb(err, data)
-      } catch (e) {
-        return cb(e)
-      }
-    }.bind(this))
-  }
-  Rule.prototype.get = function(id) {
-    return this.store.get(id)
-  }
-  Rule.prototype.create = function(f, data, cb) {
-    try {
-      var newId = Store.newId()
-        , id = ((typeof f === 'function') ? f(newId) : ((typeof f === 'string') ? f+newId : newId))
-      this.store.set(id, data)
-      cb()
-    } catch(e) {
-      cb(e)
-    }
-  }
-  Rule.prototype.update = function(id, data, cb) {
-    try {
-      this.store.set(id, data)
-      cb()
-    } catch(e) {
-      cb(e)
-    }
-  }
-  Rule.prototype.remove = function(id, cb) {
-    try {
-      this.store.del(id)
-      cb()
-    } catch(e) {
-      cb(e)
-    }
-  }
-  return Rule
-}())
+module.exports = {
+  Rule : require('./lib/Rule'),
+  RuleEngine : require('./lib/RuleEngine'),
+  RuleParam : require('./lib/RuleParam')
+}
