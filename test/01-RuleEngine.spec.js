719d365091b923c4566d9d3b149d87428e06fc9e
Nicolas Lochet <nicolas.lochet@gmail.com>
2018-03-20 00:29:44 +0100

    fix toDTO + inc deps

--- test/01-RuleEngine.spec.js
+++ test/01-RuleEngine.spec.js
@@ -116,14 +116,14 @@ test('rule-engine execute command with chatroom rules: create msg join I:join ms
     t.deepEqual(ev.stringify(), 'S: '+d.toISOString()+'-0000 chatroom:join {"chatId":"'+chatId+'","userId":"bar"}')
 
     ev = await rgine.execute(new Command(d = new Date(), 0, 'chatroom:join', {chatId, userId: 'bar'}))
-    t.deepEqual(ev.stringify(), 'I: '+d.toISOString()+'-0000 chatroom:join {"chatId":"'+chatId+'","userId":"bar"} Chatroom> member "bar" cannot join: already in')
+    t.deepEqual(ev.stringify(), 'I: '+d.toISOString()+'-0000 chatroom:join {"chatId":"'+chatId+'","userId":"bar"} Chatroom%3E%20member%20%22bar%22%20cannot%20join%3A%20already%20in')
 
     ev = await rgine.execute(new Command(d = new Date(), 0, 'chatroom:send', { chatId, userId: 'bar', msg: 'yo'}))
     t.deepEqual(chatroom, { members: [ 'foo', 'bar' ], messages: [ '* Chatroom created by foo', 'foo: test', '* Chatroom joined by bar', 'bar: yo' ] })
     t.deepEqual(ev.stringify(), 'S: '+d.toISOString()+'-0000 chatroom:send {"chatId":"'+chatId+'","userId":"bar","msg":"yo"}')
 
     ev = await rgine.execute(new Command(d = new Date(), 0, 'chatroom:err', {}))
-    t.deepEqual(ev.stringify(), 'E: '+d.toISOString()+'-0000 chatroom:err {} ERROR!\nError: ERROR!\n    at Rule.run (/home/n3/sources/repo/wool-rule/test/test-rule-chatroom.js:104:11)\n    at Rule.apply (/home/n3/sources/repo/wool-rule/lib/Rule.js:61:16)\n    at RuleEngine.execute (/home/n3/sources/repo/wool-rule/lib/RuleEngine.js:35:20)\n    at <anonymous>\n    at process._tickCallback (internal/process/next_tick.js:188:7)')
+    t.deepEqual(ev.stringify(), 'E: '+d.toISOString()+'-0000 chatroom:err {} ERROR!%0AError%3A%20ERROR!%0A%20%20%20%20at%20Rule.run%20(%2Fhome%2Fn3%2Fsources%2Frepo%2Fwool-rule%2Ftest%2Ftest-rule-chatroom.js%3A104%3A11)%0A%20%20%20%20at%20Rule.apply%20(%2Fhome%2Fn3%2Fsources%2Frepo%2Fwool-rule%2Flib%2FRule.js%3A61%3A16)%0A%20%20%20%20at%20RuleEngine.execute%20(%2Fhome%2Fn3%2Fsources%2Frepo%2Fwool-rule%2Flib%2FRuleEngine.js%3A35%3A20)%0A%20%20%20%20at%20%3Canonymous%3E%0A%20%20%20%20at%20process._tickCallback%20(internal%2Fprocess%2Fnext_tick.js%3A188%3A7)')
 
     ev = await rgine.execute(new Command(d = new Date(), 0, 'chatroom:send', { chatId, userId: 'foo', msg: 'bye'}))
     t.deepEqual(chatroom, { members: [ 'foo', 'bar' ], messages: [ '* Chatroom created by foo', 'foo: test', '* Chatroom joined by bar', 'bar: yo', 'foo: bye' ] })
@@ -142,10 +142,10 @@ test('rule-engine execute command with chatroom rules: create msg join I:join ms
     t.deepEqual(ev.stringify(), 'S: '+d.toISOString()+'-0000 chatroom:leave {"chatId":"'+chatId+'","userId":"bar"}')
 
     ev = await rgine.execute(new Command(d = new Date(), 0, 'chatroom:send', { chatId, userId: 'bar', msg: 'yo'}))
-    t.deepEqual(ev.stringify(), 'I: '+d.toISOString()+'-0000 chatroom:send {"chatId":"'+chatId+'","userId":"bar","msg":"yo"} Chatroom> member "bar" cannot send message: not in')
+    t.deepEqual(ev.stringify(), 'I: '+d.toISOString()+'-0000 chatroom:send {"chatId":"'+chatId+'","userId":"bar","msg":"yo"} Chatroom%3E%20member%20%22bar%22%20cannot%20send%20message%3A%20not%20in')
 
     ev = await rgine.execute(new Command(d = new Date(), 0, 'chatroom:leave', { chatId, userId: 'foo'}))
-    t.deepEqual(ev.stringify(), 'I: '+d.toISOString()+'-0000 chatroom:leave {"chatId":"'+chatId+'","userId":"foo"} Chatroom> member "foo" cannot leave: not in')
+    t.deepEqual(ev.stringify(), 'I: '+d.toISOString()+'-0000 chatroom:leave {"chatId":"'+chatId+'","userId":"foo"} Chatroom%3E%20member%20%22foo%22%20cannot%20leave%3A%20not%20in')
 
   } catch(e) {
     t.fail(e.message +'\n'+ e.stack)
@@ -193,7 +193,7 @@ test('rule-engine replay event with chatroom rules: create msg join I:join msg m
     ev = await rgine.replay(new Event(d = new Date(), 0, 'chatroom:join', {chatId, userId: 'bar'}, 'I', 'Chatroom> member "bar" cannot join: already in'))
     t.deepEqual(chatroom, { members: [ 'foo', 'bar' ], messages: [ '* Chatroom created by foo', 'foo: test', '* Chatroom joined by bar' ] })
     t.deepEqual(userBar.membership.length, 1)
-    t.deepEqual(ev.stringify(), 'I: '+d.toISOString()+'-0000 chatroom:join {"chatId":"'+chatId+'","userId":"bar"} Chatroom> member "bar" cannot join: already in')
+    t.deepEqual(ev.stringify(), 'I: '+d.toISOString()+'-0000 chatroom:join {"chatId":"'+chatId+'","userId":"bar"} Chatroom%3E%20member%20%22bar%22%20cannot%20join%3A%20already%20in')
 
     ev = await rgine.replay(new Event(d = new Date(), 0, 'chatroom:send', { chatId, userId: 'bar', msg: 'yo'}, 'S'))
     t.deepEqual(chatroom, { members: [ 'foo', 'bar' ], messages: [ '* Chatroom created by foo', 'foo: test', '* Chatroom joined by bar', 'bar: yo' ] })
@@ -203,7 +203,7 @@ test('rule-engine replay event with chatroom rules: create msg join I:join msg m
     t.deepEqual(chatroom, { members: [ 'foo', 'bar' ], messages: [ '* Chatroom created by foo', 'foo: test', '* Chatroom joined by bar', 'bar: yo', 'foo: bye' ] })
 
     ev = await rgine.replay(new Event(d = new Date(), 0, 'chatroom:err', {}, 'E', 'ERROR!\nError: ERROR!\n    at Rule.run (/home/n3/sources/repo/wool-rule/test/test-rule-chatroom.js:104:11)\n    at Rule.apply (/home/n3/sources/repo/wool-rule/lib/Rule.js:57:16)\n    at RuleEngine.execute (/home/n3/sources/repo/wool-rule/lib/RuleEngine.js:35:20)\n    at <anonymous>\n    at process._tickCallback (internal/process/next_tick.js:188:7)'))
-    t.deepEqual(ev.stringify(), 'E: '+d.toISOString()+'-0000 chatroom:err {} ERROR!\nError: ERROR!\n    at Rule.run (/home/n3/sources/repo/wool-rule/test/test-rule-chatroom.js:104:11)\n    at Rule.apply (/home/n3/sources/repo/wool-rule/lib/Rule.js:57:16)\n    at RuleEngine.execute (/home/n3/sources/repo/wool-rule/lib/RuleEngine.js:35:20)\n    at <anonymous>\n    at process._tickCallback (internal/process/next_tick.js:188:7)')
+    t.deepEqual(ev.stringify(), 'E: '+d.toISOString()+'-0000 chatroom:err {} ERROR!%0AError%3A%20ERROR!%0A%20%20%20%20at%20Rule.run%20(%2Fhome%2Fn3%2Fsources%2Frepo%2Fwool-rule%2Ftest%2Ftest-rule-chatroom.js%3A104%3A11)%0A%20%20%20%20at%20Rule.apply%20(%2Fhome%2Fn3%2Fsources%2Frepo%2Fwool-rule%2Flib%2FRule.js%3A57%3A16)%0A%20%20%20%20at%20RuleEngine.execute%20(%2Fhome%2Fn3%2Fsources%2Frepo%2Fwool-rule%2Flib%2FRuleEngine.js%3A35%3A20)%0A%20%20%20%20at%20%3Canonymous%3E%0A%20%20%20%20at%20process._tickCallback%20(internal%2Fprocess%2Fnext_tick.js%3A188%3A7)')
 
     ev = await rgine.replay(new Event(d = new Date(), 0, 'chatroom:leave', { chatId, userId: 'foo'}, 'S'))
     t.deepEqual(chatroom, { members: [ 'bar' ], messages: [ '* Chatroom created by foo', 'foo: test', '* Chatroom joined by bar', 'bar: yo', 'foo: bye', '* Chatroom left by foo' ] })
@@ -221,13 +221,13 @@ test('rule-engine replay event with chatroom rules: create msg join I:join msg m
     t.deepEqual(chatroom, { members: [ ], messages: [ '* Chatroom created by foo', 'foo: test', '* Chatroom joined by bar', 'bar: yo', 'foo: bye', '* Chatroom left by foo', '* Chatroom left by bar' ] })
     t.deepEqual(userFoo.membership.length, 0)
     t.deepEqual(userBar.membership.length, 0)
-    t.deepEqual(ev.stringify(), 'I: '+d.toISOString()+'-0000 chatroom:send {"chatId":"'+chatId+'","userId":"bar","msg":"yo"} Chatroom> member "bar" cannot send message: not in')
+    t.deepEqual(ev.stringify(), 'I: '+d.toISOString()+'-0000 chatroom:send {"chatId":"'+chatId+'","userId":"bar","msg":"yo"} Chatroom%3E%20member%20%22bar%22%20cannot%20send%20message%3A%20not%20in')
 
     ev = await rgine.replay(new Event(d = new Date(), 0, 'chatroom:leave', { chatId, userId: 'foo'}, 'I', 'Chatroom> member "foo" cannot leave: not in'))
     t.deepEqual(chatroom, { members: [ ], messages: [ '* Chatroom created by foo', 'foo: test', '* Chatroom joined by bar', 'bar: yo', 'foo: bye', '* Chatroom left by foo', '* Chatroom left by bar' ] })
     t.deepEqual(userFoo.membership.length, 0)
     t.deepEqual(userBar.membership.length, 0)
-    t.deepEqual(ev.stringify(), 'I: '+d.toISOString()+'-0000 chatroom:leave {"chatId":"'+chatId+'","userId":"foo"} Chatroom> member "foo" cannot leave: not in')
+    t.deepEqual(ev.stringify(), 'I: '+d.toISOString()+'-0000 chatroom:leave {"chatId":"'+chatId+'","userId":"foo"} Chatroom%3E%20member%20%22foo%22%20cannot%20leave%3A%20not%20in')
 
   } catch(e) {
     t.fail(e.message +'\n'+ e.stack)
