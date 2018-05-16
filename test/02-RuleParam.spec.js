725897b37bf6080114ceead5d7c02eb640cc44a2
Nicolas Lochet <nicolas.lochet@gmail.com>
2018-05-16 06:03:00 +0200

    fix broken fluent interface of str.regex/crypto

--- test/02-RuleParam.spec.js
+++ test/02-RuleParam.spec.js
@@ -18,22 +18,35 @@ const test = require('tape')
 test('rule-param STR', async function(t) {
   let check = RuleParam.STR('str')
     , store = new Store()
-  
+
   t.ok(await check.validate(store, { str: 'foo' }))
   t.notOk(await check.validate(store, { str: 42 }))
-  t.notOk(await check.validate(store, { foo: 'bar' }))  
+  t.notOk(await check.validate(store, { foo: 'bar' }))
   t.plan(3)
+  t.end()
+})
+
+test('rule-param STR.regex', async function(t) {
+  let check = RuleParam.STR('str').regex(/^f/)
+    , store = new Store()
+
+  t.ok(await check.validate(store, { str: 'foo' }))
+  t.notOk(await check.validate(store, { str: 'bar' }))
+  t.notOk(await check.validate(store, { str: 'another' }))
+  t.notOk(await check.validate(store, { str: 42 }))
+  t.notOk(await check.validate(store, { foo: 'bar' }))
+  t.plan(5)
   t.end()
 })
 
 test('rule-param ENUM', async function(t) {
   let check = RuleParam.ENUM('str', [ 'foo', 'bar', 'another' ])
     , store = new Store()
-  
+
   t.ok(await check.validate(store, { str: 'foo' }))
   t.ok(await check.validate(store, { str: 'another' }))
   t.notOk(await check.validate(store, { str: 42 }))
-  t.notOk(await check.validate(store, { foo: 'bar' }))  
+  t.notOk(await check.validate(store, { foo: 'bar' }))
   t.plan(4)
   t.end()
 })
